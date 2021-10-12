53b5401cc984360be81e03873133dec84944c39c
Admin <admin@olawunmi.local>
2021-10-11 23:03:27 -0400

    push to repo

--- create_infrastructure.sh
+++ create_infrastructure.sh
@@ -8,14 +8,14 @@ terraform apply --auto-approve
 #terraform destroy --auto-approve
 
 # CREATE TOOL SERVER AMI
-cd ..
-cd packer
-ls
-/usr/bin/packer fmt tool_server_ami.pkr.hcl
-/usr/bin/packer init tool_server_ami.pkr.hcl
-/usr/bin/packer build tool_server_ami.pkr.hcl
-/usr/bin/packer init agents_ami.pkr.hcl
-/usr/bin/packer build agents_ami.pkr.hcl
+#cd ..
+#cd packer
+#ls
+#/usr/bin/packer fmt tool_server_ami.pkr.hcl
+#/usr/bin/packer init tool_server_ami.pkr.hcl
+#/usr/bin/packer build tool_server_ami.pkr.hcl
+#/usr/bin/packer init agents_ami.pkr.hcl
+#/usr/bin/packer build agents_ami.pkr.hcl
 
 # CREATE SERVERS
 cd ..
@@ -38,6 +38,7 @@ cd ansible-job
 ls
 export ANSIBLE_HOSTS=inventory/ec2.py
 export EC2_INI_PATH=inventory/ec2.ini
+ls inventory/
 sudo chmod 755 inventory/ec2.py
 sudo chmod 755 inventory/ec2.ini
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playbookbroker.yml -i inventory/ec2.py
