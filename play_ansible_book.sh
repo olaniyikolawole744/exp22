487ae9c604b2526644301fc66d0ef29f3246b3a4
Admin <admin@olawunmi.local>
2021-10-11 23:35:15 -0400

    push to repo

--- play_ansible_book.sh
+++ play_ansible_book.sh
@@ -3,10 +3,10 @@
 # PLAY ANSIBLE PLAYBOOK 
 cd ansible-job
 ls
-export ANSIBLE_HOSTS=inventory/ec2.py
-export EC2_INI_PATH=inventory/ec2.ini
-cd inventory
+export ANSIBLE_HOSTS=inventory/hosts/ec2.py
+export EC2_INI_PATH=inventory/hosts/ec2.ini
+cd inventory/hosts
 ls 
-sudo chmod 755 inventory/ec2.py
-sudo chmod 755 inventory/ec2.ini
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playbookbroker.yml -i inventory/ec2.py
+sudo chmod 755 inventory/hosts/ec2.py
+sudo chmod 755 inventory/hosts/ec2.ini
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playbookbroker.yml -i inventory/hosts/ec2.py
