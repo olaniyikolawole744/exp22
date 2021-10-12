979b6416da43575a91189fae1f7ae0a4fe039463
Admin <admin@olawunmi.local>
2021-10-11 23:09:49 -0400

    push to repo

--- create_infrastructure.sh
+++ create_infrastructure.sh
@@ -39,7 +39,8 @@ ls
 
 export ANSIBLE_HOSTS=inventory/ec2.py
 export EC2_INI_PATH=inventory/ec2.ini
-ls inventory/
+cd inventory
+ls 
 sudo chmod 755 inventory/ec2.py
 sudo chmod 755 inventory/ec2.ini
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playbookbroker.yml -i inventory/ec2.py
